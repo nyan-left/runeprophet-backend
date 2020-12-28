31d3c0584cce907bb747280379c2c0d56c521424
nyan-left <deim.jak@gmail.com>
2020-12-28 16:22:26 +0000

    Don't attempt fix inject if already have

--- fix-sls-offline.js
+++ fix-sls-offline.js
@@ -1,9 +1,21 @@
-let fs = require('fs');
-let lineNumber = 6;
-let data = fs.readFileSync('node_modules/hapi/lib/defaults.js').toString().split("\n");
-data.splice(lineNumber, 0, `Os.tmpDir = Os.tmpdir;`);
-let text = data.join("\n");
+const fs = require('fs');
+const LINE_NUMBER = 6;
+const FILE_LOCATION = 'node_modules/hapi/lib/defaults.js'
+const INJECT_FIX_STRING = `Os.tmpDir = Os.tmpdir;`
 
-fs.writeFile('node_modules/hapi/lib/defaults.js', text, (err) => {
-  if (err) return console.log(err);
-});
+let data = fs.readFileSync(FILE_LOCATION)
+if (data.includes(INJECT_FIX_STRING)) {
+  console.log("Skipping fix injection, already exists.")
+} else {
+  data = data.toString().split("\n");
+  data.splice(LINE_NUMBER, 0, INJECT_FIX_STRING);
+  let text = data.join("\n");
+
+  fs.writeFile(FILE_LOCATION, text, (err) => {
+    if (err) {
+      return console.log(err);
+    } else {
+      return console.log('Injected fix successfully')
+    }
+  });
+}
