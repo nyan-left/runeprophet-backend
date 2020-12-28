6e212a718d2e696d499dbe14dc25f8d512b59730
nyan-left <deim.jak@gmail.com>
2020-12-28 19:15:47 +0000

    Increase timeout to 30s

--- handler.py
+++ handler.py
@@ -1,47 +1,29 @@
 import os
 os.environ[ 'MPLCONFIGDIR' ] = '/tmp/'
-try:
-  if os.environ.get("AWS_EXECUTION_ENV") is not None:
+
+if os.environ.get("AWS_EXECUTION_ENV") is not None:
+  try:
     import unzip_requirements
-  import pandas as pd
-  from fbprophet import Prophet
-  import json
-except(Exception):
-  pass
+  except ImportError:
+    pass
+  
+import pandas as pd
+from fbprophet import Prophet
+import json
 
 def predict(event, context):
-  try:
-    data = json.loads(event['body'])
-    df = pd.DataFrame(data)
-    df.head()
-    m = Prophet()
-    m.fit(df)
-    future = m.make_future_dataframe(periods=30)
-    future.tail()
-    forecast = m.predict(future)
-    forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
-    jsonResponse = forecast.to_json(orient='records', date_format='iso')
-    print('Trying to return a response')
-    print(jsonResponse[0:30])
-    response = {
-        "statusCode": 200,
-        "body": jsonResponse
-    }
-    return response
-  except Exception:
-    pass
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+  data = json.loads(event['body'])
+  df = pd.DataFrame(data)
+  df.head()
+  m = Prophet()
+  m.fit(df)
+  future = m.make_future_dataframe(periods=30)
+  future.tail()
+  forecast = m.predict(future)
+  forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+  jsonResponse = forecast.to_json(orient='records', date_format='iso')
+  response = {
+      "statusCode": 200,
+      "body": jsonResponse
+  }
+  return response
